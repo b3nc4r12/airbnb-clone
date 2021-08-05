d147420e3638c4f4dd5bf02298c456b82cbd63a8
Ben Carter <benjcarter06@gmail.com>
2021-08-04 22:56:42 -0300

    day 2 build

--- components/Banner.js
+++ components/Banner.js
@@ -9,12 +9,14 @@ const Banner = () => {
                 layout="fill"
                 objectFit="cover"
             />
-            <Fade bottom>
-                <div className="absolute top-1/2 w-full text-center">
-                    <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
-                    <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">I'm flexible</button>
-                </div>
-            </Fade>
+            <div className="h-full w-full flex items-center justify-center">
+                <Fade bottom>
+                    <div className="flex flex-col items-center justify-center w-full text-center">
+                        <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
+                        <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">I'm flexible</button>
+                    </div>
+                </Fade>
+            </div>
         </div>
     )
 }
