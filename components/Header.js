8749cd7a52aed793de1d29e36bc24a142f09634c
Ben Carter <benjcarter06@gmail.com>
2021-08-04 22:50:35 -0300

    day 2 build

--- components/Header.js
+++ components/Header.js
@@ -3,18 +3,22 @@ import {
     SearchIcon,
     GlobeAltIcon,
     MenuIcon,
-    UserCircleIcon
+    UserCircleIcon,
+    XIcon
 } from "@heroicons/react/solid"
 import { signOut, useSession } from "next-auth/client"
+import { useState } from "react"
 
 const Header = () => {
     const [session] = useSession();
 
+    const [searchStatus, setSearchStatus] = useState(false);
+
     return (
         <header className="sticky top-0 z-50">
-            <div className="grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+            <div className={!searchStatus ? "grid grid-cols-2 sm:grid-cols-3 md:px-10 bg-white shadow-md p-5" : "flex bg-white shadow-md p-5"}>
                 {/* Left - Airbnb Logo */}
-                <div className="relative flex items-center h-10 cursor-pointer my-auto">
+                <div className={!searchStatus ? "flex relative items-center h-10 cursor-pointer my-auto" : "hidden"}>
                     <Image
                         src="https://links.papareact.com/qd3"
                         layout="fill"
@@ -24,33 +28,40 @@ const Header = () => {
                 </div>
 
                 {/* Middle - Search */}
-                <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+                <div className={!searchStatus ? "hidden sm:flex items-center md:border-2 rounded-full py-2 md:shadow-sm" : "flex items-center border-2 rounded-full py-2 shadow-sm flex-grow"}>
                     <input
                         className="ml-5 flex-grow bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
                         type="text"
                         placeholder="Start your search"
                     />
-                    <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
+                    <SearchIcon className={!searchStatus ? "hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" : "inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer mx-2"} />
                 </div>
 
                 {/* Right */}
                 <div className="flex items-center justify-end space-x-4 text-gray-500">
-                    <p className="hidden md:inline cursor-pointer">Become a host</p>
-                    <GlobeAltIcon className="h-6 cursor-pointer" />
-                    <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
-                        <MenuIcon className="h-6 cursor-pointer mr-2" />
-                        {session?.user.image ?
-                            <Image
-                                src={session.user.image}
-                                height={24}
-                                width={24}
-                                onClick={signOut}
-                                className="rounded-full cursor-pointer"
-                            />
-                            :
-                            <UserCircleIcon onClick={signOut} className="h-6 cursor-pointer" />
-                        }
-                    </div>
+                    {searchStatus ?
+                        <XIcon onClick={() => setSearchStatus(false)} className="h-7 ml-5 cursor-pointer" />
+                        :
+                        <>
+                            <p className="hidden md:inline cursor-pointer">Become a host</p>
+                            <SearchIcon onClick={() => setSearchStatus(true)} className="sm:hidden h-6 cursor-pointer" />
+                            <GlobeAltIcon className="h-6 cursor-pointer" />
+                            <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+                                <MenuIcon className={`h-6 cursor-pointer ${session?.user.image && "mr-2"}`} />
+                                {session?.user.image ?
+                                    <Image
+                                        src={session.user.image}
+                                        height={24}
+                                        width={24}
+                                        onClick={signOut}
+                                        className="rounded-full cursor-pointer"
+                                    />
+                                    :
+                                    <UserCircleIcon onClick={signOut} className="h-6 cursor-pointer" />
+                                }
+                            </div>
+                        </>
+                    }
                 </div>
             </div>
         </header>
